8b0ce2224feb44567c4c13e218102e59e7c941d1
taehongkim <public.bluemiv@gmail.com>
2022-04-18 18:59:07 +0900

    order 앱에 button 추가

--- packages/order/src/App.js
+++ packages/order/src/App.js
@@ -5,6 +5,7 @@ function App() {
         <div>
             <h1>Order Application</h1>
             <Button type="primary">테스트 버튼</Button>
+            <Button type="primary">테스트 버튼2</Button>
         </div>
     );
 }
